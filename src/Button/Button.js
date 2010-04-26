4e403818de0cb5d07c07e73309b7f69a85351d39
Bogdan Gusiev <agresso@gmail.com>
2010-04-26 11:49:42 +0300

    Documentation for Button and Widget

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,18 +1,44 @@
 
 
+/**
+ *  Button widget suppose to wrap any clickable element
+ *  @class Button
+ *  @for Button
+ *  @extends Vf.Widget
+ *  @namespace Vf
+ */
 Vf.Button = new Class({
   Extends: Vf.Widget,
 
+  options: {
+    hideOnClick: false
+  },
 
+
+  /**
+   * Constructor
+   * Valid options:
+   *   * all options from Vf.Widget class
+   *   * hideOnClick - autohides element on click. Default: false.
+   * @constructor
+   * @param {Mixed} element The DOM element or element id to be wraped under widget class.
+   * @param {Hash} options hash with options
+   */
   initialize: function(element, options) {
     this.parent(element, options);
     this.addEventToElement('click');
   },
 
 
+  /*
+   *  Implementation
+   */
   click: function(event) {
     if (event) {
       event.stop();
+    }
+    if (this.options.hideOnClick) { 
+      this.hide();
     }
     this.fireEvent('click');
   }
--- src/Widget/Widget.js
+++ src/Widget/Widget.js
@@ -1,3 +1,14 @@
+/**
+ * Wrapping namespace for all widgets
+ * @module Vf
+ */
+
+/**
+ * Base class for all widgets. Provides many utility methods
+ * @class Widget
+ * @for Widget
+ * @namespace vf
+ */
 Vf.Widget = new Class({
 
   Extends: Vf.Stateful('hidden', 'hide', 'show', 'setHidden'),
@@ -12,10 +23,14 @@ Vf.Widget = new Class({
   property: 'widget',
 
   options: {
-    animator: Vf.Animators.generic,
-    styles: { }
   },
 
+  /**
+   * Constructor
+   * @constructor
+   * @param {Mixed} element The DOM element or element id to be wraped under widget class.
+   * @param {Hash} options Widget options
+   */
   initialize: function(element, options) { 
     this.enableLog();
     this.element = $(element);
@@ -29,14 +44,18 @@ Vf.Widget = new Class({
     if ($chk(options)) {
       this.setOptions(options);
     }
-    this.setStyles(this.options.styles);
-    this.fireEvent('instantiate');
   },
 
   /*
   *  API
   */
 
+  /**
+   * Replaces current widget element with new received element
+   * and initialize current widget over new element
+   * @method reinitialize
+   * @param {String|Element} elementOrHtmlString with html or element to replace
+   */
   reinitialize: function(elementOrHtmlString) {
     var element = Vf.Utils.toElement(elementOrHtmlString);
     if (this.element.parentNode) {
@@ -45,108 +64,229 @@ Vf.Widget = new Class({
     this.initialize(element);
   },
 
+  /**
+   *  Adds ui-hidden class to element
+   *  and do the hide animated
+   *  @method hide
+   *  @return {Boolean} true if widget was not hidden and false if it was
+   */
   hide: function() {
     var result = this.parent.apply(this, arguments);
-    if (result) {
-      this.options.animator.hide(this.element);
-    }
+    //if (result) {
+      //this.options.animator.hide(this.element);
+    //}
     return result;
   },
 
+  /**
+   *  Removes ui-hidden class from element
+   *  and do the show animation
+   *  @method show
+   *  @return {Boolean} true if widget was not shown and false if it was
+   */
   show: function() {
     var result = this.parent.apply(this, arguments);
-    if (result) {
-      this.options.animator.show(this.element);
-    }
+    //if (result) {
+      //this.options.animator.show(this.element);
+    //}
     return result;
   },
 
+  /**
+   * Gets first nested element that match the query
+   * @method getElement
+   * @param {String} selector 
+   * @return {Element} queried element
+   */
   getElement: function(query) {
     return this.element.getElement(query);
   },
 
+  /**
+   * Gets all nested elements that match the query
+   * @method getElements
+   * @param {String} selector 
+   * @return {Array} queried elements array
+   */
   getElements: function(query) {
     return this.element.getElements(query);
   },
 
-  grab: function(item, where) {
-    return this.element.grab(item, where);
+  /**
+   *  Grabs given element and append to the list of childs for widget
+   *  @method grab
+   *  @param {Element} element
+   *  @param {String} position of the element. Could be 'top' or bottom'. Default 'bottom'
+   *  @return {Vf.Widget} This widget
+   */
+  grab: function(element, where) {
+    this.element.grab(element, where);
+    return this;
   },
 
+  /**
+   *  Appends given list of element to the list of childs for widget
+   *  @method adopt
+   *  @param {Array|Element} elementOrArrayOfElements could be array of elements. Can accept any number of arguments.
+   *  @return {Vf.Widget} This widget
+   */
   adopt: function() {
-    return this.element.adopt(arguments);
+    this.element.adopt(arguments);
+    return this;
   },
 
+  /**
+   *  Appends given html string to the bottom of inner html
+   *  @method append
+   *  @param {String} htmlString
+   *  @return {Vf.Widget} this widget
+   */
   append: function(text) {
     return this.adopt(Vf.Utils.toHtml(text));
   },
 
-  addClass: function(name) {
-    return this.element.addClass(name);
+  /**
+   *  Applies given class to the widget element if it is not applied yet
+   *  @method addClass
+   *  @param {String} className
+   *  @return {Vf.Widget} this widget
+   */
+  addClass: function(className) {
+    this.element.addClass(className);
+    return this;
   },
 
+  /**
+   *  Removes the given class from the widget element
+   *  @method removeClass
+   *  @param {String} className
+   *  @return {Vf.Widget} this widget
+   */
   removeClass: function(name) {
-    return this.element.removeClass(name);
+    this.element.removeClass(name);
+    return this;
   },
 
+  /**
+   *  Impties all children of the element and removes it from DOM and garbages the element
+   *  @return {undefined}
+   */
   destroy: function() {
     this.hide();
     this.element.destroy();
     this.fireEvent('destroy');
   },
 
+  /**
+   *  Gets attribute of widget element
+   *  @param {String} attribute
+   *  @return {String} Attribute value
+   */
   get: function(attr) {
     return this.element.get(attr);
   },
 
+  /**
+   *  Sets attribute of widget element
+   *  @param {String} attribute
+   *  @param {Mixed} value
+   *  @return {Vf.Widget} this widget
+   */
   set: function(attr, value) {
-    return this.element.set(attr, value);
+    this.element.set(attr, value);
+    return this;
   },
 
+  /**
+   *  Sets the styles attribute of the element
+   *  @param {Object} styles hash 
+   *  @return {Vf.Widget} this widget
+   */
   setStyles: function(styles) {
-    return this.element.setStyles(styles);
+    this.element.setStyles(styles);
+    return this;
   },
 
-  setStyle: function(style, value) {
-    return this.element.setStyle(style, value);
+  /**
+   *  Sets the specified style attribute of the element to the given value
+   *  @param {String} attribute
+   *  @param {Mixed} value
+   *  @return {Vf.Widget} this widget
+   */
+  setStyle: function(attribute, value) {
+    this.element.setStyle(attribute, value);
+    return this;
   },
 
+  /**
+   *  Removes all child nodes of the widget element
+   *  @return {Vf.Widget} this widget
+   */
   empty: function() {
-    return this.element.empty();
+    this.element.empty();
+    return this;
   },
 
-  toElement: function() {
-    return this.element;
-  },
-
+  /**
+   *  Gets widget element size
+   *  @return {Dimention} 
+   */
   getSize: function() {
     return this.element.getSize();
   },
 
+  /**
+   *  Gets the id attribute of the widget element
+   *  @return {String} id
+   */
   getId: function() {
     return this.element.id;
   },
 
-  addClass: function(clazz) {
-    return this.element.addClass(clazz);
+  /**
+   *  Determines if the widget element has specified class
+   *  @param {String} className
+   *  @return {Boolean}
+   */
+  hasClass: function(className) {
+    return this.element.hasClass(className);
   },
 
-  hasClass: function(clazz) {
-    return this.element.hasClass(clazz);
-  },
-
+  /**
+   *  Gets parent element to the widget element
+   *  @return {Element} parent element
+   */
   getParent: function() {
     return this.element.getParent();
   },
 
+  /**
+   *  Gets the inner html of the widget element
+   *  @return {String} inner html
+   */
   getHtml: function() {
     return this.get('html');
   },
 
+  /**
+   *  Sets the inner html of the widget element
+   *  @param {String} htmlString
+   *  @return {Vf.Widget} this widget
+   */
   setHtml: function(html) {
-    return this.set('html', html);
+    this.set('html', html);
+    return this;
   },
 
+  /**
+   *  Manages the 'ui-hidden' class on the widget element
+   *  Toggles class if the value is undefined
+   *  removes the class if the value is true 
+   *  removes the class if the value is false 
+   *  @param {Boolean} value
+   *  @returns {Boolean} true if the ui state was chaged and false otherwise
+   *
+   */
   toggle: function(value) {
     return this.setHidden($defined(value) ? !value : undefined);
   },
@@ -159,8 +299,11 @@ Vf.Widget = new Class({
     $A(arguments).each(function(event) {
       this.element.addEvent(event, this[event].bind(this));
     }.bind(this));
-  }
+  },
 
+  toElement: function() {
+    return this.element;
+  }
 
 });
 

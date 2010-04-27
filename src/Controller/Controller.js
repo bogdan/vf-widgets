0249cddfdd05bf7ad2a0d38f09167fbc4dcd410e
Bogdan Gusiev <agresso@gmail.com>
2010-04-27 11:41:32 +0300

    Add Controller widget documentation

--- src/Controller/Controller.js
+++ src/Controller/Controller.js
@@ -1,16 +1,66 @@
+/**
+ *  Controller is a widget that controllers other widget 
+ *  (in most cases nested within the controller)
+ *  Controller provides simple DSL to define and initialize it's widgets
+ *  @class Controller
+ *  @extends Vf.Widget
+ *  @namespace Vf
+ */
 Vf.Controller = new Class({
 
   Extends: Vf.Widget,
 
+  /**
+   *  Valid widget options
+   *  <ul>
+   *    <li>all Vf.Widget options</li>
+   *    <li>
+   *      useSpinner - determines if the controller uses spinner to indicate activity
+   *      bypass this option to all defined widgets contructor. Default: false.
+   *    </li>
+   *    <li>spinnerTarget - element to draw a spinner. Default: this widget element.</li>
+   *  </ul>
+   *  @property options
+   */
   options: {
     useSpinner: false,
     spinnerTarget: null //initialized in constructor
   },
 
+  /**
+   *  Contains a hash of widget descriptors.
+   *  Each descriptor is used to build a widget and assign it to the property of controller object
+   *  Descriptor key is used as the name of the property.
+   *  <h4>Example</h4>
+   *  <code>widgets: { submitButton: {clazz: Vf.Button, selector: '.js-submit'}}</code>
+   *  this interpreted in the constructor as 
+   *  <code>this.submitButton = new Vf.Button(this.getElement('.js-submit'))</code>
+   *  <h4>Widget descriptor options</h4>
+   *  <ul>
+   *    <li>clazz - widget class that used as contructor. Default: Vf.Widget.</li>
+   *    <li>selector - query that is used to find the widget. Default: null.</li>
+   *    <li>
+   *      global - if true the query is applied to whole document scope 
+   *      otherwise only to relatively to current widget element. Default false
+   *    </li>
+   *    <li>
+   *      multiple - if true finds all widgets that match query and wrap all of them with widget. Default: false.
+   *    </li>
+   *    <li>required - if true throws an error if element not found. Ignored if multiple is set to true. Default: false.</li>
+   *  </ul>
+   *  IMPORTANT: other options are forwarded to described widget constructor
+   *  @property widgets
+   */
   widgets: {
   },
 
 
+  /**
+   *  Constructor
+   *  @constructor
+   *  @param {Element|String} element
+   *  @param {Hash} options
+   */
   initialize: function(element, options) {
     this.options.spinnerTarget = $(element);
     this.parent.apply(this, arguments);
@@ -21,16 +71,30 @@ Vf.Controller = new Class({
   *  Implementation
   */
 
+  /**
+   *  Initialize every widget defined in #widgets property
+   *  @method initWidgets
+   */
   initWidgets: function() {
     this.buildWidgets(this.widgets, {useSpinner: this.options.useSpinner, spinnerTarget: this.options.spinnerTarget});
   },
 
+  /**
+   *  Initialize every widget from the given descriptors hash
+   *  @param {Hash} widgets hash of widget descriptors
+   *  @param {Hash} defaultOptions options that got merged to widget constructor options
+   */
   buildWidgets: function(widgets, defaultOptions) {
     new Hash(widgets).each(function(options, property) {
       this.buildWidget(property, $merge(defaultOptions, options));
     }.bind(this));
   },
 
+  /**
+   *  Builds a widget from the descriptor and options
+   *  @param {String} property property to assign the constructed widget class
+   *  @param {Hash} widget descriptor
+   */
   buildWidget: function(property, options) {
     var selector = options.selector;
 
